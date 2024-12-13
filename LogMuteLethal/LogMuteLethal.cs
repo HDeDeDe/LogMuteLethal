151d8015d5982c274e8cac3869ea1e900a2cc8dd
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-12-12 16:17:59 -0800

    Move reveal to another class

--- LogMuteLethal/LogMuteLethal.cs
+++ LogMuteLethal/LogMuteLethal.cs
@@ -9,21 +9,6 @@ using Unity.Netcode;
 
 namespace HDeMods {
     public static class LogMuteLethal {
-#if DEBUG
-        private static Hook infoHook;
-        private static Hook warningHook;
-        private static Hook errorHook;
-        
-        private delegate void logDel(Action<object> orig, object message);
-        private static logDel logEv;
-        
-        private static Hook infoHookContext;
-        private static Hook warningHookContext;
-        private static Hook errorHookContext;
-        
-        private delegate void logDelContext(Action<object, UnityEngine.Object> orig, object message, UnityEngine.Object context);
-        private static logDelContext logEvContext;
-#endif
         private static ILHook networkVarDirtyHook;
         
         // Thanks to .score for providing these 2 functions
@@ -44,42 +29,15 @@ namespace HDeMods {
 
             networkVarDirtyHook = new ILHook(AccessTools.Method(typeof(NetworkVariableBase),
                 nameof(NetworkVariableBase.SetDirty)), NetworkVariableBase_SetDirty);
-#if DEBUG
-            logEv += RevealThyself;
-            infoHook = new Hook(AccessTools.Method(typeof(Debug), nameof(Debug.Log), 
-                new Type[] { typeof(object) }), logEv);
-            warningHook = new Hook(AccessTools.Method(typeof(Debug), nameof(Debug.LogWarning), 
-                new Type[] { typeof(object) }), logEv);
-            errorHook = new Hook(AccessTools.Method(typeof(Debug), nameof(Debug.LogError), 
-                new Type[] { typeof(object) }), logEv);
-            
-            logEvContext += RevealThyselfContext;
-            infoHookContext = new Hook(AccessTools.Method(typeof(Debug), nameof(Debug.Log), 
-                new Type[] { typeof(object), typeof(UnityEngine.Object) }), logEvContext);
-            warningHookContext = new Hook(AccessTools.Method(typeof(Debug), nameof(Debug.LogWarning), 
-                new Type[] { typeof(object), typeof(UnityEngine.Object) }), logEvContext);
-            errorHookContext = new Hook(AccessTools.Method(typeof(Debug), nameof(Debug.LogError), 
-                new Type[] { typeof(object), typeof(UnityEngine.Object) }), logEvContext);
-#endif
-        }
-#if DEBUG
-        private static void RevealThyself(Action<object> orig,object message) {
-            System.Reflection.MethodBase method = (new System.Diagnostics.StackTrace()).GetFrame(2).GetMethod();
-            MUTE.Log.Debug(method.ReflectedType?.Name + "." + method.Name);
-            orig(message);
+            Reveal.Startup();
         }
         
-        private static void RevealThyselfContext(Action<object, UnityEngine.Object> orig,object message, UnityEngine.Object context) {
-            System.Reflection.MethodBase method = (new System.Diagnostics.StackTrace()).GetFrame(2).GetMethod();
-            MUTE.Log.Debug(method.ReflectedType?.Name + "." + method.Name);
-            orig(message, context);
-        }
-#endif
         private static void HUDManager_SetPlayerLevelSmoothly(ILContext il) {
             ILCursor c = new ILCursor(il);
             
             c.RemoveLog(nameof(Debug.Log));
         }
+        
         private static void HoarderBugAI_SetGoTowardsTargetObject(ILContext il) {
             ILCursor c = new ILCursor(il);
             
---
+++ LogMuteLethal/Reveal.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+using MonoMod.RuntimeDetour;
+using HarmonyLib;
+
+namespace HDeMods {
+    public static class Reveal {
+#if DEBUG
+        private static Hook infoHook;
+        private static Hook warningHook;
+        private static Hook errorHook;
+        
+        private delegate void logDel(Action<object> orig, object message);
+        private static logDel logEv;
+        
+        private static Hook infoHookContext;
+        private static Hook warningHookContext;
+        private static Hook errorHookContext;
+        
+        private delegate void logDelContext(Action<object, UnityEngine.Object> orig, object message, UnityEngine.Object context);
+        private static logDelContext logEvContext;
+#endif
+        public static void Startup() {
+#if DEBUG
+            logEv += Thyself;
+            infoHook = new Hook(AccessTools.Method(typeof(Debug), nameof(Debug.Log), 
+                new Type[] { typeof(object) }), logEv);
+            warningHook = new Hook(AccessTools.Method(typeof(Debug), nameof(Debug.LogWarning), 
+                new Type[] { typeof(object) }), logEv);
+            errorHook = new Hook(AccessTools.Method(typeof(Debug), nameof(Debug.LogError), 
+                new Type[] { typeof(object) }), logEv);
+            
+            logEvContext += ThyselfContext;
+            infoHookContext = new Hook(AccessTools.Method(typeof(Debug), nameof(Debug.Log), 
+                new Type[] { typeof(object), typeof(UnityEngine.Object) }), logEvContext);
+            warningHookContext = new Hook(AccessTools.Method(typeof(Debug), nameof(Debug.LogWarning), 
+                new Type[] { typeof(object), typeof(UnityEngine.Object) }), logEvContext);
+            errorHookContext = new Hook(AccessTools.Method(typeof(Debug), nameof(Debug.LogError), 
+                new Type[] { typeof(object), typeof(UnityEngine.Object) }), logEvContext);
+#endif
+        }
+#if DEBUG
+        private static void Thyself(Action<object> orig,object message) {
+            System.Reflection.MethodBase method = (new System.Diagnostics.StackTrace()).GetFrame(2).GetMethod();
+            
+            if (method.ReflectedType?.Name == "LogMessage") MUTE.Log.Debug(new System.Diagnostics.StackTrace());
+            else MUTE.Log.Debug(method.ReflectedType?.Name + "." + method.Name);
+            
+            orig(message);
+        }
+        
+        private static void ThyselfContext(Action<object, UnityEngine.Object> orig,object message, UnityEngine.Object context) {
+            System.Reflection.MethodBase method = (new System.Diagnostics.StackTrace()).GetFrame(2).GetMethod();
+            
+            if (method.ReflectedType?.Name == "LogMessage") MUTE.Log.Debug(new System.Diagnostics.StackTrace());
+            else MUTE.Log.Debug(method.ReflectedType?.Name + "." + method.Name);
+            
+            orig(message, context);
+        }
+#endif
+    }
+}
