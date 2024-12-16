84e58a67f87e4b040ca979d7f5806fac28ae7230
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-12-16 12:26:21 -0800

    IT FUCKING WORKS! LET'S GOOOOOOOOOOOO!

--- LogMuteLethal/LogMuteUnity.cs
+++ LogMuteLethal/LogMuteUnity.cs
@@ -26,25 +26,45 @@ namespace HDeMods {
             UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
         
         public static void Startup() {
-            Process currentProc = Process.GetCurrentProcess();
-            IntPtr baseAddress = currentProc.Modules.Cast<ProcessModule>()
+        Process currentProc = Process.GetCurrentProcess();
+        IntPtr baseAddress = currentProc.Modules.Cast<ProcessModule>()
                 .Single(m => m.ModuleName == "UnityPlayer.dll")
                 .BaseAddress;
             
-            if (!NopLocation(currentProc, baseAddress, 0xB9EE00, 0xe8_8b_f0_4b_00_48_8b_9c)) 
+            //0xe8_8b_f0_4b_00_48_8b_9c
+            if (!NopLocation(currentProc, baseAddress, 0xB9FA00, 0x9c_8b_48_00_4b_f0_8b_e8)) 
                 MUTE.Log.Error("Failed to silence Audio Source!");
-            if (!NopLocation(currentProc, baseAddress, 0x0EF489, 0xe8_02_ea_f6_00_0f_28_bc)) 
+#if DEBUG
+            else MUTE.Log.Warning("Silenced Audio Source");
+#endif
+            //0xe8_02_ea_f6_00_0f_28_bc
+            if (!NopLocation(currentProc, baseAddress, 0xF0089, 0xbc_28_0f_00_f6_ea_02_e8)) 
                 MUTE.Log.Error("Failed to silence Look Zero!");
-            if (!NopLocation(currentProc, baseAddress, 0xB9F62B, 0xe8_60_e8_4b_00_40_38_7d)) 
-                MUTE.Log.Error("Failed to silence Filter!");
-            if (!NopLocation(currentProc, baseAddress, 0xA34511, 0xe8_7a_99_62_00_80_bd_18)) 
+#if DEBUG
+            else MUTE.Log.Warning("Silenced Look Zero");
+#endif    
+            //0xe8_60_e8_4b_00_40_38_7d
+            if (!NopLocation(currentProc, baseAddress, 0xBA022b, 0x7d_38_40_00_4b_e8_60_e8)) 
+                MUTE.Log.Error("Failed to silence Custom Filter!");
+#if DEBUG
+            else MUTE.Log.Warning("Silenced Custom Filter");
+#endif
+            //0xe8_7a_99_62_00_80_bd_18
+            if (!NopLocation(currentProc, baseAddress, 0xA35111, 0x18_bd_80_00_62_99_7a_e8)) 
                 MUTE.Log.Error("Failed to silence NavMesh!");
-            if (!NopLocation(currentProc, baseAddress, 0xA393B9, 0xe8_d2_4a_62_00_32_c0_48)) 
+#if DEBUG
+            else MUTE.Log.Warning("Silenced NavMesh");
+#endif
+            //0xe8_d2_4a_62_00_32_c0_48
+            if (!NopLocation(currentProc, baseAddress, 0xA39FB9, 0x48_c0_32_00_62_4a_d2_e8)) 
                 MUTE.Log.Error("Failed to silence SetDestination!");
+#if DEBUG
+            else MUTE.Log.Warning("Silenced SetDestination");
+#endif
         }
         
         private static bool NopLocation(Process currentProc, IntPtr baseAddress, nint offset, ulong instrToReplace) {
-            ulong mask = 0xff_ff_ff_ff_ff_00_00_00;
+            ulong mask = 0xff_ff_ff_00_00_00_00_00;
             
             byte* logCall = (byte*)(baseAddress + offset);
             
@@ -52,11 +72,18 @@ namespace HDeMods {
                 MUTE.Log.Error("No Match. " + (*(long*)logCall).ToString("X") + " " + ((long)instrToReplace).ToString("X"));
                 return false;
             }
-            mask ^= instrToReplace;
+            mask &= instrToReplace;
+            //0x66_48_90_48_90_00_00_00
+            mask |= 0x00_00_00_90_48_90_48_66;
+
+            uint oldProt;
+
+            // 0x40 - PAGE_EXECUTE_READWRITE
+            VirtualProtectEx(currentProc.Handle, (IntPtr)logCall, (UIntPtr)8, 0x40, out oldProt);
+            *(ulong*)logCall = mask;
+            VirtualProtectEx(currentProc.Handle, (IntPtr)logCall, (UIntPtr)8, oldProt, out _);
             
-            *(ulong*)logCall = 0x66_48_90_48_90_00_00_00 + mask;
-            
-            if (FlushInstructionCache(currentProc.Handle, logCall, 5) == 0) return false;
+            if (FlushInstructionCache(currentProc.Handle, logCall, 8) == 0) return false;
             return true;
         }
     }
